d8f4d3338e40a342ea136067117aaa147f05ee21
hdabaghi <hdabaghi@leyton.com>
2024-10-04 21:48:32 +0100

    [TD-1] : Create the project.

---
+++ src/test/java/hamzadabaghi/testingdemo/TestingDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package hamzadabaghi.testingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TestingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
